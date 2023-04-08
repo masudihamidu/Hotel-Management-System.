b9447cbf50992bec4fb7b86e1a8cf48a971cbc93
masudihamidu <masudihmfaume@gmail.com>
2023-04-08 19:37:59 +0300

    adding form container and other component

--- src/hotelmanagementsystem/HotelManagementSystem.java
+++ src/hotelmanagementsystem/HotelManagementSystem.java
@@ -4,41 +4,20 @@
  * and open the template in the editor.
  */
 package hotelmanagementsystem;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JLabel;
+
 /**
  *
  * @author I.A.T COMPUTERS LLC
  */
-public class HotelManagementSystem extends JFrame{
+public class HotelManagementSystem {
 
     /**
      * @param args the command line arguments
      */
-    //constructor
-    HotelManagementSystem(){
-        JButton systemAdmin = new JButton("System admin");
-        JButton roomAssistant = new JButton("Room assistant");
-        JLabel containerTitle = new JLabel("Hotel management System");
-        
-        systemAdmin.setBounds(50,80,170,50);
-        roomAssistant.setBounds(50,150,170,50);
-        containerTitle.setBounds(50,20,200,50);
-        
-        add(systemAdmin);
-        add(roomAssistant);
-        add(containerTitle);
-        
-        setSize(300,400);
-        setLayout(null);
-        setVisible(true);
-        
-    }
-    
     public static void main(String[] args) {
         // TODO code application logic here
-        new HotelManagementSystem();
+        IndexPage frame = new IndexPage();
+        frame.show();
     }
     
 }
