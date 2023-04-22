e57080cfbbd6a3319d6b0069867c61329371a415
masudihamidu <masudihmfaume@gmail.com>
2023-04-22 14:59:58 +0300

    Creating room assistance details

---
+++ src/hotelmanagementsystem/RoomAssistanceDetails.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagementsystem;
+
+/**
+ *
+ * @author I.A.T COMPUTERS LLC
+ */
+public class RoomAssistanceDetails extends User {
+   String Email;
+   String gender;
+   String EmployeeID;
+   
+   
+           }
