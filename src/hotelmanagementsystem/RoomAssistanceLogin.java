56554f0dda7b81fcb3cfb21d5194d8d013d22cc5
masudihamidu <masudihmfaume@gmail.com>
2023-06-17 21:05:57 +0300

    modify login method and invoked to another class

--- src/hotelmanagementsystem/RoomAssistanceLogin.java
+++ src/hotelmanagementsystem/RoomAssistanceLogin.java
@@ -161,33 +161,15 @@ public class RoomAssistanceLogin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        DBConnection conn = new DBConnection();
+        //DBConnection conn = new DBConnection();
         Registration registrationDetails = new Registration();
+        String username = txtUserName.getText();
+        String password = new String(txtPassword.getPassword());
+        String sqlStatment = "SELECT firstname, password FROM roomassistance WHERE firstname = '"+username+"'AND password = '"+password+"'";
+        
+        registrationDetails.login(username, password, sqlStatment);
                 
-          try{
-            String username = txtUserName.getText();
-            String password = new String(txtPassword.getPassword());
-//            String enteredEncrypted =  registrationDetails.encryption(password);
-                       
-            String sqlStatment = "SELECT firstname, password FROM roomassistance WHERE firstname = '"+username+"'AND password = '"+password+"'";
-            ResultSet result = conn.statment.executeQuery(sqlStatment);
-            if(result.next()){
-                String uname = result.getString("firstname");
-                String pwd = result.getString("password");               
-   
-                if((username.equals(uname)) && (registrationDetails.encryption(password).equals(pwd))){  
-                  new RoomAssistanceDashboard().setVisible(true);                 
-                  this.setVisible(false);
-                }
-                 
-            }
-            else{
-                JOptionPane.showMessageDialog(null, "Username or password does not match");
-            }
-        }
-        catch(Exception e){
-            JOptionPane.showMessageDialog(null, e);
-        }
+         
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jLabel6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel6MouseClicked
